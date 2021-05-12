e017a7d7342e3594a1fd5bc064504ccfef8b1396
Kacper <kacpergierycz@gmail.com>
2021-05-12 16:02:36 +0200

    Jea!!! JDBC works fine

--- JDBC/src/jdbcStart/DemoClass.java
+++ JDBC/src/jdbcStart/DemoClass.java
@@ -1,10 +1,43 @@
 package jdbcStart;
 
+import java.sql.*;
+
+
+/*
+1. import---->java.sql
+2. load and register the driver--- com.mysql.Driver
+3.Create connection --->Connection
+4.create a statement---> Statement
+5.sequre the query-->
+6.proces the result-->
+7.close
+*/
+
+
+
 public class DemoClass {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws Exception {
+		
+		String url="jdbc:mysql://localhost:3306/aliens";
+		String uname="root";
+		String pass="Zamiec01";
+		String query="SELECT * FROM aliens.student;";
+		
+		
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection con= DriverManager.getConnection(url, uname, pass);
+		Statement st= con.createStatement();
+		ResultSet rs=st.executeQuery(query);
+		
+		rs.next();
+		String name= rs.getString("username");
+		
+		System.out.println(name);
+		
+		st.close();
+		con.close();
+		
 	}
 
 }
