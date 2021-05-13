bc2c33a7a76343e9c9cadf83e0aac0de02a6c58a
Kacper <kacpergierycz@gmail.com>
2021-05-13 14:22:58 +0200

    DAO add,delete,get

--- JDBC/src/jdbcStart/DemoClass.java
+++ JDBC/src/jdbcStart/DemoClass.java
@@ -60,7 +60,13 @@ public class DemoClass {
 //		con.close();
 		
 		StudentDAO dao= new  StudentDAO();		
-		Student s1=dao.getStudent(2);
+		Student s2=new Student();
+				s2.userId=11;
+				s2.name="Arachna";
+				dao.connect();
+			//	dao.addStudent(s2);
+		Student s1=dao.getStudent(1);
+		dao.removeStudent(s2);
 		System.out.println(s1.name);
 		
 
@@ -68,20 +74,33 @@ public class DemoClass {
 }
 	
 	class StudentDAO{
-
-		public Student getStudent(int userId) {
+		
+		String url = "jdbc:mysql://localhost:3306/aliens";
+		String uname = "hbstudent";
+		String pass = "hbstudent";
+		
+		
+		Connection con=null;
+		public void connect() {
 			
-			String url = "jdbc:mysql://localhost:3306/aliens";
-			String uname = "hbstudent";
-			String pass = "hbstudent";
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+				 con = DriverManager.getConnection(url, uname, pass);
+		
+			} catch (Exception ex) {
+			System.out.println(ex);
+			}		
+		}
+		
+
+		public Student getStudent(int userId) {			
 		
 		try {
 			String query = "select username from student where userId="+userId;
 			Student s= new Student();
 			s.userId=userId;	
 
-			Class.forName("com.mysql.jdbc.Driver");
-			Connection con = DriverManager.getConnection(url, uname, pass);
+
 			Statement st=con.createStatement();
 			ResultSet rs= st.executeQuery(query);
 			rs.next();
@@ -89,6 +108,7 @@ public class DemoClass {
 			s.name=name;
 			
 			return s;
+			
 			} catch (Exception ex) {
 			System.out.println(ex);
 			}
@@ -96,6 +116,39 @@ public class DemoClass {
 			return null;
 	
 		}
+	
+		public void addStudent(Student s) {
+			
+			String query = "insert into Student values (?,?)";
+			PreparedStatement pst;
+			try {
+				pst = con.prepareStatement(query);
+				pst.setInt(1, s.userId);
+				pst.setString(2,s.name);
+				pst.executeUpdate();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	
+			
+		}
+		
+		public void removeStudent(Student s) {
+			String query = "delete from Student where userId=?";
+			PreparedStatement pst;
+			try {
+				pst = con.prepareStatement(query);
+				pst.setInt(1, s.userId);
+			//	pst.setString(2,s.name);
+				pst.executeUpdate();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+		
 	}
 
 	class Student{
